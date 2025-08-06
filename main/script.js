ede38028dacc8d0f6f56187140867003bc680af4
RpnAndri <andri.crisostomo@gmail.com>
2025-08-07 01:06:21 +0800

    add: structure

--- main/script.js
+++ main/script.js
@@ -1,23 +1,56 @@
-import * as THREE from 'three';
+onload = () =>{
+    div = document.querySelector("div.container");
+    div.classList.remove("container");
+};
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+intro = document.querySelector("div.intro");
+intro.style.display = "block";
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+flowers = document.querySelector("div.all-flowers");
+flowers.style.display = "none";
 
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+btn_yes = document.querySelector("button#yes");
+btn_no = document.querySelector("button#no");
 
-camera.position.z = 5;
-
-function animate() {
-  renderer.render( scene, camera );
+// Create an Audio object
+const audio = new Audio("music.mp3");
+audio.loop = true;
+audio.volume = 0.5;
+function yes() {
+    intro.style.display = "none";
+    flowers.style.display = "block";
+    audio.currentTime = 0;
+    audio.play();
 }
-renderer.setAnimationLoop( animate );
 
-console.log("hello world");
+font_size = 21;
+counter = 0;
+function no() {
+    console.log(btn_yes.style.fontSize);
+    btn_yes.style.fontSize = `${font_size*1.2}px`;
+    btn_yes.style.width = "fit-content";
+    font_size *= 1.2;
+
+    counter += 1;
+    switch(counter) {
+        case 1:
+            alert("HEY WHY NO?!??!");
+            break;
+        case 2:
+            alert("are you serious right neow?");
+            break;
+        case 3:
+            alert("hahaha game is over now");
+            break;
+        case 4:
+            alert("cmon now... wh-whyy? :(");
+            break;
+        case 5:
+            alert("okay... as you wish...");
+            break;
+        default:
+            alert("umalis ka na >:(");
+            break;
+    }
+}
